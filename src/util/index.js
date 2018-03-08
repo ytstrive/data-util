5191997a8325ff91293711b4089022681d7c5056
小朋友 <ytstrive@sina.com>
2018-03-08 17:06:17 +0800

    add mergeRecursive
    新增深度拷贝方法.

--- src/util/index.js
+++ src/util/index.js
@@ -2,7 +2,9 @@ import pro from '../prototype';
 export default {
     /**
      * 计算一组数据各占百分比
-     * @param {Array|Object} data 
+     * @param {Array|Object} [data=[]] 
+     * @param {Number} [fixed=2] 
+     * @returns 
      */
     scale(data = [], fixed = 2) {
         const type = pro.toString.call(data);
@@ -26,5 +28,29 @@ export default {
             });
             return obj;
         })();
+    },
+    /**
+     * 深度拷贝
+     * @param {Object} obj1 
+     * @param {Object} obj2 
+     * @returns 
+     */
+    mergeRecursive(obj1, obj2) {
+        for (let p in obj2) {
+            try {
+                if (obj2[p].constructor === Object) {
+                    if (!obj1[p]) {
+                        obj1[p] = {}
+                    }
+                    obj1[p] = this.mergeRecursive(obj1[p], obj2[p])
+                } else {
+                    obj1[p] = obj2[p]
+                }
+            } catch (e) {
+
+            }
+        }
+
+        return obj1;
     }
 }
--- test/calculate.test.js
+++ test/calculate.test.js
@@ -22,3 +22,26 @@ describe('util.scale', () => {
         console.log(scale);
     })
 })
+describe('util.mergeRecursive', () => {
+
+    it('scale.mergeRecursive.1', () => {
+        let data = {
+            a: 'a',
+            b: 1,
+            c: false,
+            d: undefined,
+            e: {
+                e1: 'e1'
+            },
+            f: function () { }
+        }, data2 = {
+            a: 'a',
+            e: {
+                e2: 'e2'
+            },
+            f: function () { console.log('---'); }
+        }
+        const mergeRecursive = util.mergeRecursive(data, data2);
+        console.log(mergeRecursive);
+    })
+})
